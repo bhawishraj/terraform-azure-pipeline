796dd3d313fa00610ea4d717e568eaec257090de
Bhawish Raj <122223653+bhawishraj@users.noreply.github.com>
2026-01-28 12:37:56 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ terraform {
 
   backend "azurerm" {
     resource_group_name  = "tfstate-rg"
-    storage_account_name = "tfstateaccount"
+    storage_account_name = "tfstatestorage9"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
   }
