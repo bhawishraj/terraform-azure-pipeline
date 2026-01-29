fe5ea9602faea490d5196c7a44133c009ed3cb07
Bhawish Raj <122223653+bhawishraj@users.noreply.github.com>
2026-01-29 11:22:39 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,70 +18,78 @@ provider "azurerm" {
   features {}
 }
 
+# ------------ Variables ------------
 variable "vm_admin_password" {
   type      = string
   sensitive = true
 }
 
+# Random suffix for non-core names
 resource "random_integer" "suffix" {
   min = 10000
   max = 99999
 }
 
-# 1. Resource Group
-resource "azurerm_resource_group" "example" {
+# ------------ Core Layer (Stable Names & Location) ------------
+
+# Resource Group (do not change name or location after creation)
+resource "azurerm_resource_group" "core_rg" {
   name     = "rg-example"
   location = "southeastasia"
 }
 
-# 2. Virtual Network & Subnet
-resource "azurerm_virtual_network" "example_vnet" {
-  name                = "vnet-${random_integer.suffix.result}"
+# Virtual Network (stable name)
+resource "azurerm_virtual_network" "core_vnet" {
+  name                = "vnet-core"
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.core_rg.location
+  resource_group_name = azurerm_resource_group.core_rg.name
 }
 
-resource "azurerm_subnet" "example_subnet" {
-  name                 = "example-subnet"
-  resource_group_name  = azurerm_resource_group.example.name
-  virtual_network_name = azurerm_virtual_network.example_vnet.name
+# Subnet (stable name)
+resource "azurerm_subnet" "core_subnet" {
+  name                 = "subnet-core"
+  resource_group_name  = azurerm_resource_group.core_rg.name
+  virtual_network_name = azurerm_virtual_network.core_vnet.name
   address_prefixes     = ["10.0.2.0/24"]
 }
 
-# 3. NICs
-resource "azurerm_network_interface" "example_nic" {
+# ------------ Compute Layer (Flexible) ------------
+
+# Network Interfaces (okay to recreate independently)
+resource "azurerm_network_interface" "vm_nic" {
   count               = 3
   name                = "nic-${count.index + 1}-${random_integer.suffix.result}"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.core_rg.location
+  resource_group_name = azurerm_resource_group.core_rg.name
 
   ip_configuration {
     name                          = "internal"
-    subnet_id                     = azurerm_subnet.example_subnet.id
+    subnet_id                     = azurerm_subnet.core_subnet.id
     private_ip_address_allocation = "Dynamic"
   }
 
-  depends_on = [azurerm_subnet.example_subnet]
+  depends_on = [azurerm_subnet.core_subnet]
 }
 
-# 4. Windows VMs
-resource "azurerm_windows_virtual_machine" "example_vm" {
+# Windows Virtual Machines
+resource "azurerm_windows_virtual_machine" "vm" {
   count               = 3
-  name                = "winvm-${count.index + 1}-${random_integer.suffix.result}"
+  name                = "vm-${count.index + 1}-${random_integer.suffix.result}"
   computer_name       = "WIN${count.index + 1}"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.core_rg.name
+  location            = azurerm_resource_group.core_rg.location
   size                = "Standard_B1ms"
   admin_username      = "azureuser"
   admin_password      = var.vm_admin_password
   network_interface_ids = [
-    azurerm_network_interface.example_nic[count.index].id
+    azurerm_network_interface.vm_nic[count.index].id
   ]
 
   os_disk {
     caching              = "ReadWrite"
     storage_account_type = "Standard_LRS"
+    # Removed disk_size_gb to use default (127 GB for Windows Server 2019)
   }
 
   source_image_reference {
@@ -92,42 +100,45 @@ resource "azurerm_windows_virtual_machine" "example_vm" {
   }
 }
 
-# 5. Storage Account & Container
-resource "azurerm_storage_account" "example_storage" {
+# ------------ Service Layer (Stable) ------------
+
+# Storage Account (stable name, unique in Azure)
+resource "azurerm_storage_account" "app_storage" {
   name                     = "stor${random_integer.suffix.result}"
-  resource_group_name      = azurerm_resource_group.example.name
-  location                 = azurerm_resource_group.example.location
+  resource_group_name      = azurerm_resource_group.core_rg.name
+  location                 = azurerm_resource_group.core_rg.location
   account_tier             = "Standard"
   account_replication_type = "GRS"
 }
 
-resource "azurerm_storage_container" "example_container" {
+resource "azurerm_storage_container" "app_container" {
   name                  = "app-data"
-  storage_account_name  = azurerm_storage_account.example_storage.name
+  storage_account_name  = azurerm_storage_account.app_storage.name
   container_access_type = "private"
 }
 
-# 6. SQL Server & Database
-resource "azurerm_mssql_server" "example_sql_server" {
-  name                         = "sqlsrv${random_integer.suffix.result}"
-  resource_group_name          = azurerm_resource_group.example.name
-  location                     = azurerm_resource_group.example.location
+# SQL Server (stable name)
+resource "azurerm_mssql_server" "app_sql_server" {
+  name                         = "sqlserver-core"
+  resource_group_name          = azurerm_resource_group.core_rg.name
+  location                     = azurerm_resource_group.core_rg.location
   version                      = "12.0"
   administrator_login          = "sqladminuser"
   administrator_login_password = "YourSecurePassword123!"
 }
 
-resource "azurerm_mssql_database" "example_sql_db" {
+# SQL Database
+resource "azurerm_mssql_database" "app_sql_db" {
   name      = "sqldb${random_integer.suffix.result}"
-  server_id = azurerm_mssql_server.example_sql_server.id
+  server_id = azurerm_mssql_server.app_sql_server.id
   sku_name  = "S0"
 }
 
-# 7. Cosmos DB & SQL Database
-resource "azurerm_cosmosdb_account" "example_cosmos" {
+# Cosmos DB Account
+resource "azurerm_cosmosdb_account" "app_cosmos" {
   name                = "cosmosacct${random_integer.suffix.result}"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.core_rg.location
+  resource_group_name = azurerm_resource_group.core_rg.name
   offer_type          = "Standard"
   kind                = "GlobalDocumentDB"
 
@@ -138,13 +149,14 @@ resource "azurerm_cosmosdb_account" "example_cosmos" {
   }
 
   geo_location {
-    location          = azurerm_resource_group.example.location
+    location          = azurerm_resource_group.core_rg.location
     failover_priority = 0
   }
 }
 
-resource "azurerm_cosmosdb_sql_database" "example_cosmos_db" {
+# Cosmos DB SQL Database
+resource "azurerm_cosmosdb_sql_database" "app_cosmos_sql_db" {
   name                = "cosmosdb${random_integer.suffix.result}"
-  resource_group_name = azurerm_resource_group.example.name
-  account_name        = azurerm_cosmosdb_account.example_cosmos.name
+  resource_group_name = azurerm_resource_group.core_rg.name
+  account_name        = azurerm_cosmosdb_account.app_cosmos.name
 }
